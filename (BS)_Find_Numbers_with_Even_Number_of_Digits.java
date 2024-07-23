1abd9a9a6938fc2bf9245d06d5b7d23e2e40b4d9
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-23 23:11:17 +0530

    Create (BS)_Find_Numbers_with_Even_Number_of_Digits.java

---
+++ (BS)_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+       
+        int count = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            int digits = 0;
+            while(nums[i]!=0){
+                nums[i]=nums[i]/10;
+                digits ++;
+            }
+            if(digits%2==0){
+                count ++;
+            }
+        }
+        return count;
+    }
+}
