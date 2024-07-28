15c1b22a866de2dd3b745104082f0e2b66082abd
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-28 19:52:05 +0530

    Update Climbing_Stairs.java

--- Climbing_Stairs.java
+++ Climbing_Stairs.java
@@ -6,8 +6,8 @@ class Solution {
         int a = 3;
         int b = 2;
         for(int i = 0; i<n-3 ; i++){
-            a = a+b;
-            b = a-b;
+            a = a+b;  // a+=b;
+            b = a-b;  
         }
         return a;
     }
