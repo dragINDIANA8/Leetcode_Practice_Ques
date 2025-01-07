75771d9f088d4c89127923656d08a8582d01e2e4
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2025-01-07 23:14:42 +0530

    (DP)Sqrt.java

--- (DP)Sqrt.java
+++ (DP)Sqrt.java
@@ -1,5 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        return(int)(Math.sqrt(x)); // one line of code for sqrt problem on leetcode 
+        return(int)(Math.sqrt(x)); // one line of code for sqrt problem on leetcode .
     }
 }
