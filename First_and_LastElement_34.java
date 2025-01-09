9f3b68457450b8aaf04d55b3bc507d43fe605e3d
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2025-01-09 19:45:21 +0530

    Update First_and_LastElement_34.java

--- First_and_LastElement_34.java
+++ First_and_LastElement_34.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int i=0;
-        int j=nums.length-1;
+
+        // Using two pointer approach to solve this problem
+        
+        int i=0;   // left index
+        int j=nums.length-1;     // right index
 
         while(i<=j){
-            if(nums[i]==target && nums[j]==target){
+            if(nums[i]==target && nums[j]==target){   // if left index and right index are equal to 
                 int a[] = {i,j};
-                return a;
+                return a;   // return the elements which are on that index
             }
-           else if(nums[i]!=target){
-               i++;
+           else if(nums[i]!=target){  // if left index not equal to target then move it to the right side
+               i++;       
             } 
-            else if(nums[j]!=target){
+            else if(nums[j]!=target){   // if right index not equal to target then move it to the left side
                 j--;
             }
     
         }
-        return new int[] {-1,-1};
+        return new int[] {-1,-1};            // retur this if it is not equal to target
     }
 }
