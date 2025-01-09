bb854f13daf8bce1e4a4a02a59dc2dff68d1632f
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2025-01-09 19:46:20 +0530

    Update Sort_Colors_75.java

--- Sort_Colors_75.java
+++ Sort_Colors_75.java
@@ -1,6 +1,8 @@
 // using bubble sort to sort colors
 class Solution {
     public void sortColors(int[] nums) {
+
+        // you can use any sorting technique to sort this. For now I have used bubble sort to sort this problem
          for(int i=0; i<nums.length; i++){
             for(int j=0; j<nums.length -1; j++){
                 if(nums[j]>nums[j+1]){
