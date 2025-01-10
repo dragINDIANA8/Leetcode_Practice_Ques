1eeee811d33e4660fedb3347472ae642b8d69b7b
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2025-01-10 17:47:06 +0530

    Update BinarySearch_704.java

--- BinarySearch_704.java
+++ BinarySearch_704.java
@@ -4,18 +4,18 @@ class Solution {
         int hi = nums.length -1; // higher index
 
         while(li<=hi){
-            int mi = (li+hi)/2; // midle index
-            if(nums[mi]==target){
+            int mi = (li+hi)/2; // middle index  or you can also write mid= li+(hi-li)/2;
+            if(nums[mi]==target){   // if our middle index is equal to the target then return the middle index
                 return mi;
             }
-            else if(nums[mi]<target){
+            else if(nums[mi]<target){      // if our middle index is smaller than target 
                 li = mi+1;
             }
             else{
-                hi = mi-1;
+                hi = mi-1;      // if our middle index is greater than target 
             }
         }
-        return -1;
+        return -1;         // return this if the target is not found
         
     }
 }
